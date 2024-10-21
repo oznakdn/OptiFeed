3ba373637ee8d480d6702fbac2ddd3faea1ab67a
oznakdn <ozanakaydin@gmail.com>
2024-10-21 20:37:14 +0300

    Add project files.

---
+++ src/OptiFeed.BlazorWeb/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddMasaBlazor(options =>
+{
+    options.ConfigureTheme(theme =>
+    {
+        theme.Dark = true;
+    });
+});
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
