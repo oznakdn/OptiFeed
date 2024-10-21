701ed1264249f735516c9f698246996147de3e7e
oznakdn <ozanakaydin@gmail.com>
2024-10-21 21:05:22 +0300

    updated

---
+++ src/OptiFeed.Core/Models/Feed.cs
@@ -0,0 +1,12 @@
+﻿namespace OptiFeed.Core.Models;
+
+public class Feed
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double DryMatter { get; set; }      // % Dry matter (Kuru Madde)
+    public double EnergyContent { get; set; }  // Mcal/kg
+    public double ProteinContent { get; set; } // g/kg
+    public double CostPerKg { get; set; }      // Cost in $ per kg
+    public double MaxUsage { get; set; }       // Max kg that can be used in the ration
+}
